68916360912e7727a9fb66d7ef1a2c6c7883e926
Davi Amaral <dasoitaperuna@gmail.com>
2026-01-13 18:46:01 -0300

    Update project structure
    Updated pom.xml to remove packaging and modules, enabled default Spring Boot auto-configuration, added @Entity annotation to Proprietario, and moved MenuApiApplicationTests.java to resources.

--- menu-api/src/main/java/com/example/menu_api/MenuApiApplication.java
+++ menu-api/src/main/java/com/example/menu_api/MenuApiApplication.java
@@ -2,9 +2,8 @@ package com.example.menu_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MenuApiApplication {
 
 	public static void main(String[] args) {
--- menu-api/src/main/java/com/example/menu_api/domain/model/Proprietario.java
+++ menu-api/src/main/java/com/example/menu_api/domain/model/Proprietario.java
@@ -1,8 +1,10 @@
 package com.example.menu_api.domain.model;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 public class Proprietario {
