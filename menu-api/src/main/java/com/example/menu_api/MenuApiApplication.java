fea7c7623a661de77d093317a96722a01c3583e1
Davi Amaral <dasoitaperuna@gmail.com>
2026-01-13 17:54:56 -0300

    Refactor project structure and update configuration
    Updated Maven groupId and dependencies, improved ProprietarioController sample data, and excluded DataSourceAutoConfiguration in MenuApiApplication. Adjusted application properties for new database name and enabled JPA settings. Also made minor IDE and encoding configuration changes.

--- menu-api/src/main/java/com/example/menu_api/MenuApiApplication.java
+++ menu-api/src/main/java/com/example/menu_api/MenuApiApplication.java
@@ -2,8 +2,9 @@ package com.example.menu_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MenuApiApplication {
 
 	public static void main(String[] args) {
--- menu-api/src/main/java/com/example/menu_api/api/controller/ProprietarioController.java
+++ menu-api/src/main/java/com/example/menu_api/api/controller/ProprietarioController.java
@@ -1,30 +1,30 @@
 package com.example.menu_api.api.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
+import com.example.menu_api.domain.model.Proprietario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.menu_api.domain.model.Proprietario;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var prop1 = new Proprietario();
-        prop1.setId(1L);
-        prop1.setNome("João Silva");
-        prop1.setEmail("teste@gmail.com");
-        prop1.setTelefone("123456789");
+        var proprietario1 = new Proprietario();
+        proprietario1.setId(1L);
+        proprietario1.setNome("João");
+        proprietario1.setTelefone("34 99999-1111");
+        proprietario1.setEmail("joaodascouves@algaworks.com");
 
-        var prop2 = new Proprietario();
-        prop2.setId(2L);
-        prop2.setNome("Maria Oliveira");
-        prop2.setEmail("maria.oliveira@gmail.com");
-        prop2.setTelefone("987654321");
+        var proprietario2 = new Proprietario();
+        proprietario2.setId(2L);
+        proprietario2.setNome("Maria");
+        proprietario2.setTelefone("11 97777-1111");
+        proprietario2.setEmail("mariadasilva@algaworks.com");
 
-        return Arrays.asList(prop1, prop2);
+        return Arrays.asList(proprietario1, proprietario2);
     }
+
 }
