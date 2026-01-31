d5c7632767bc69a61f789c2e9449caafe91c5bfb
Davi Amaral <dasoitaperuna@gmail.com>
2026-01-31 11:11:30 -0300

    Improve validation error handling in ApiExceptionHandler
    Enhanced the handleMethodArgumentNotValid method to include a map of invalid fields and their error messages in the response. Also renamed the NegocioException handler method to 'capturar' for clarity.

--- menu-api/src/main/java/com/example/menu_api/api/exceptionhandler/ApiExceptionHandler.java
+++ menu-api/src/main/java/com/example/menu_api/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,10 +2,12 @@ package com.example.menu_api.api.exceptionhandler;
 
 import com.example.menu_api.domain.exception.NegocioException;
 import org.jspecify.annotations.Nullable;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +15,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -21,16 +25,23 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                             HttpHeaders headers, HttpStatusCode status,
                                                                             WebRequest request) {
-
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos!");
         problemDetail.setType(URI.create("https://example.com/erros/campos-invalidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        objectError -> ((FieldError)objectError).getField(),
+                        DefaultMessageSourceResolvable::getDefaultMessage));
+
+        problemDetail.setProperty("fields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
 
     @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<String> handleException(NegocioException ex) {
+    public ResponseEntity<String> capturar(NegocioException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
